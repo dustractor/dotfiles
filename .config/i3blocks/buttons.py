9b7aa357fc885940f04688b0b83a29e869f901e8
Shams Kitz <dustractor@gmail.com>
2022-05-31 15:56:08 -0500

    all

--- .config/i3blocks/buttons.py
+++ .config/i3blocks/buttons.py
@@ -5,17 +5,22 @@ with open(data_path,"r") as f:
     data = f.readlines()
 
 has_colon = re(".*:.*").match
+has_pipe = re(".*\|.*").match
 for ln in data:
+    icon = ""
     line = ln.strip()
     if line:
         if has_colon(line):
             name,_,cmd = line.partition(":")
         else:
             name = cmd = line
-        fmt = f"""[{name}]
+        if has_pipe(name):
+            icon,_,name = name.partition("|")
+        fmt = f'''[{name}]
 name={name}
-full_text={name}
+full_text={name if not icon else icon}
+align=left
 command=[[ -z "$BLOCK_BUTTON" ]] || i3-msg -q exec {cmd}
-"""
+'''
         print(fmt)
 
